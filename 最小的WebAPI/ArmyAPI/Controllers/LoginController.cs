6d26935ce532fe30baed60d6b00fd280e13e7502
Alex.Chen <Alexc_Chen@syscom.com.tw>
2023-10-02 01:47:12 +0800

    新增 LoginController.cs，Check 函式，比對帳號、密碼(MD5 再 BASE64)

---
+++ 最小的WebAPI/ArmyAPI/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArmyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        // GET: HomeController
+        [HttpGet]
+        public IActionResult Login()
+        {
+            // 實現登錄邏輯
+            return Ok("Login successful!");
+        }
+
+        [HttpGet("Check")]
+        public bool Check(string n, string p)
+        {
+            string acc = "Army";
+            string pw = "kU/9esipmrbMKMhJKgJQmg=="; //ArmyAdmin
+
+            bool result = false;
+
+            if (n == acc && MD5EncryptionBASE64Encode(p) == pw)
+                result = true;
+
+            return result;
+        }
+
+        private string MD5EncryptionBASE64Encode(string s)
+        {
+            System.Security.Cryptography.MD5 hs = System.Security.Cryptography.MD5.Create();
+            byte[] db = hs.ComputeHash(System.Text.Encoding.UTF8.GetBytes(s));
+            return Convert.ToBase64String(db);
+
+        }
+    }
+}
