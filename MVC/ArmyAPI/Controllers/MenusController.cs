276040e5ced79580461a6a676f1089d693811c4a
Alex.Chen <Alexc_Chen@syscom.com.tw>
2023-10-24 18:05:43 +0800

    新增 Menus Controller (未完成)

---
+++ MVC/ArmyAPI/Controllers/MenusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArmyAPI.Controllers
+{
+    public class MenusController : Controller
+    {
+        // GET: Menus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
