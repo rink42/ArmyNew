e2ea4ec5289a8965d57587d510576c8c8e8acd71
Alex.Chen <Alexc_Chen@syscom.com.tw>
2023-10-30 23:44:28 +0800

    加入 CORS

--- MVC/ArmyAPI/Global.asax.cs
+++ MVC/ArmyAPI/Global.asax.cs
@@ -19,5 +19,28 @@ namespace ArmyAPI
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
+
+		protected void Application_BeginRequest()
+		{
+			if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+			{
+				Response.Flush();
+			}
+		}
+
+		protected void Application_PreSendRequestHeaders()
+		{
+			Response.Headers.Remove("Access-Control-Allow-Origin");
+			Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+			if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+			{
+				Response.Headers.Remove("Access-Control-Allow-Methods");
+				Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+				Response.Headers.Remove("Access-Control-Allow-Headers");
+				Response.Headers.Add("Access-Control-Allow-Headers", "*");
+			}
+		}
 	}
 }
